31e57c4dc634df30d86301ef56fcf295032c808d
Jean-Louis Huynen <huynenjl@gmail.com>
2021-03-08 15:44:35 +0100

    add: [MISP] wip queries

--- MISP_export/query.py
+++ MISP_export/query.py
@@ -10,8 +10,23 @@ redis_graph = Graph('pretensor', r)
 def getBinaryCC(sha256):
     print(sha256)
     query = """MATCH (bin:Binary {{sha256: "{}"}})<-[h1:host]-(c1:CC)
-               RETURN c1""".format(sha256)
+               RETURN c1, bin""".format(sha256)
+    return redis_graph.query(query)
+
+def getBinarySiblings(sha256, depth):
+    print(sha256)
+    query = """MATCH (b1:Binary {{sha256: "{}"}})<-[:host]-(cc1:CC)-[:host]->(b2:Binary)
+               WHERE b2.sha256 <> ""
+               WITH b1, b2, collect(DISTINCT cc1) as commonCCs
+               WHERE size(commonCCs) >= {} 
+               RETURN b2, commonCCs""".format(sha256, depth)
+    return redis_graph.query(query)
+
+def getBinariesCCs():
+    query = """MATCH (bin:Binary)<-[h1:host]-(c1:CC)
+               RETURN c1, bin"""
     return redis_graph.query(query)
 
 # Print resultset
-getBinaryCC("7e9002d8a7bc8364f291e8774c3bfd794ba496cdf0e2823513247d87d582382a").pretty_print()
+# getBinaryCC("fc888339e8cf0ad37a20f56acc247e374cbe960aeafb541fc374776c68a467f1").pretty_print()
+getBinarySiblings("8dec3f18d8652bc68fbfcf516cb0bcabf259aaf61c65044d917bce9640be2db4", 10).pretty_print()
