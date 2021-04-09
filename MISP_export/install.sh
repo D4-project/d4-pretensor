792fb921d63324b77d374856a1307844e829f89f
Jean-Louis Huynen <huynenjl@gmail.com>
2021-04-09 12:07:16 +0200

    chg: [install] fix pydeep install

--- MISP_export/install.sh
+++ MISP_export/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install libfuzzy-dev
 virtualenv -p python3 serv-env
 . ./serv-env/bin/activate
-pip3 install -U flask Flask-AutoIndex redisgraph pymisp lief python-magic
+pip install -U flask Flask-AutoIndex redisgraph pymisp lief python-magic 
+pip install git+https://github.com/kbandla/pydeep.git
