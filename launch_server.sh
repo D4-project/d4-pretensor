ca497a12c15e9819687a290d1f80325a78199889
Jean-Louis Huynen <huynenjl@gmail.com>
2021-04-20 15:13:44 +0200

    chg: [doc] a bit of documentation

--- launch_server.sh
+++ launch_server.sh
@@ -13,12 +13,12 @@ screen -dmS "pretensor"
 sleep 0.1
 
 screen -S "pretensor" -X screen -t "pew-redis" bash -c "(${DIR}/redis/src/redis-server ${DIR}/redis.conf); read x;"
-#screen -S "pretensor" -X screen -t "redis-insight" bash -c "(${DIR}/redisinsight-linux64-1.10.0); read x;"
+screen -S "pretensor" -X screen -t "redis-insight" bash -c "(${DIR}/redisinsight-linux64-1.10.0); read x;"
 
 sleep 0.5
 
-redis-cli -h localhost -p 6502 -n 0 GRAPH.QUERY pretensor "CALL db.idx.fulltext.createNodeIndex('Bot', 'firstseen')"
 redis-cli -h localhost -p 6502 -n 0 GRAPH.QUERY pretensor "CALL db.idx.fulltext.createNodeIndex('Bot', 'hostname')"
-redis-cli -h localhost -p 6502 -n 0 GRAPH.QUERY pretensor "CALL db.idx.fulltext.createNodeIndex('CC', 'firstseen')"
+redis-cli -h localhost -p 6502 -n 0 GRAPH.QUERY pretensor "CALL db.idx.fulltext.createNodeIndex('Command', 'content')"
+redis-cli -h localhost -p 6502 -n 0 GRAPH.QUERY pretensor "CALL db.idx.fulltext.createNodeIndex('Binary', 'binname')"
 
 exit 0
